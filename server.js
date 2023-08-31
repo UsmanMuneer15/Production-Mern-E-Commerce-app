aacc3e50e942547d3c098d240b7947dd0a14ca9a
Usman Muneer <129029349+UsmanMuneer15@users.noreply.github.com>
2023-08-31 00:44:32 -0700

    Update server.js

--- server.js
+++ server.js
@@ -16,8 +16,8 @@ dotenv.config();
 //database config
 connectDB();
 //esmodule fix
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 
 //rest object
@@ -27,7 +27,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-// app.use(express.static(path.join(__dirname,'./client/build')));
+app.use(express.static(path.join(__dirname,'./client/build')));
 
 //routes
 app.use("/api/v1/auth", authController);
@@ -36,9 +36,9 @@ app.use("/api/v1/category", categoryRoutes);
 
 app.use("/api/v1/product",productRoutes)
 //rest api
-// app.use('*',function(req,res){
-//   res.sendFile(path.join(__dirname,'./client/build/index.html'));
-// })
+app.use('*',function(req,res){
+  res.sendFile(path.join(__dirname,'./client/build/index.html'));
+})
 
 //port
 const PORT = process.env.PORT || 8000;
